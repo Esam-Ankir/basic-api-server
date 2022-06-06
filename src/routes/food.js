925cffeee40970370143dada27acc8625f1953d3
Esam-Ankir <eankir@hotmail.com>
2022-06-06 19:31:08 +0300

    second commit

--- src/routes/food.js
+++ src/routes/food.js
@@ -1,48 +1,50 @@
-"use strict";
-const express = require("express");
-const { Food } = require("../models/food");
+'use strict';
+const express = require('express');
+const { Food } = require('../models/index');
 const foodRouter = express.Router();
-const validator = require('../middleware/validator');
+// const validator = require('../middleware/validator');
 
-foodRouter.get("/food", getFood);
-foodRouter.get("/food/:id", getOneFood);
-foodRouter.post("/food", createFood);
-foodRouter.put("/food/:id", updateFood);
-foodRouter.delete("/food/:id", deleteFood);
+foodRouter.get('/food', getFoods);
+// app.get("/food", validator, (req, res) => {getFoods});
+foodRouter.get('/food/:id', getOneFood);
+foodRouter.post('/food', addOneFood);
+foodRouter.put('/food/:id', updateFood);
+foodRouter.delete('/food/:id', deleteFood);
 
-async function getFood(req, res) {
-    const allFood = await Food.findAll();
-    res.status(200).json(allFood);
+async function getFoods(req, res) {
+  const foods = await Food.findAll();
+  res.status(200).json(foods);
 }
-
 async function getOneFood(req, res) {
-    const foodId = parseInt(req.params.id);
-    let Food = await Food.findOne({ where: { id: foodId } });
-    res.status(200).json(Food);
+  const foodId = parseInt(req.params.id);
+  const food = await Food.findOne({ where: { id: foodId } });
+  res.status(200).json(food);
 }
-
-async function createFood(req, res) {
-    let newFood = req.body;
-    let Food = await Food.create(newFood);
-    res.status(201).json(Food);
+async function addOneFood(req, res) {
+  const newFood = req.body;
+  const food = await Food.create(newFood);
+  res.status(201).json(food);
 }
-
 async function updateFood(req, res) {
-    let foodId = parseInt(req.params.id);
-    let updateFood = req.body; 
-    let foundFood = await Food.findOne({ where: { id: foodId } });
-    if (foundFood) {
-        let updatedFood = await foundFood.update(updateFood);
-        res.status(201).json(updatedFood);
-    } else {
-        res.status(404);
-    }
+  const foodId = parseInt(req.params.id);
+  const updateFood = req.body;
+  const foundFood = await Food.findOne({ where: { id: foodId } });
+  if (foundFood) {
+    const updatedFood = await foundFood.update(updateFood);
+    res.status(201).json(updatedFood);
+  } else {
+    res.status(404).json({ message: 'Food not found' });
+  }
 }
 async function deleteFood(req, res) {
-    let foodId = parseInt(req.params.id);
-    let deleteFood = await Food.destroy({ where: { id: foodId } });
-    res.status(204).json(deleteFood); 
+  const foodId = parseInt(req.params.id);
+  const foundFood = await Food.findOne({ where: { id: foodId } });
+  if (foundFood) {
+    await foundFood.destroy();
+    res.status(204).json({ message: 'Food deleted' });
+  } else {
+    res.status(404).json({ message: 'Food not found' });
+  }
 }
 module.exports = foodRouter;
-
 
